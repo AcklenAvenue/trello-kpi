138776cc2da2873b54bb09501af34277ee3322c2
Arturo Cordova <atu.cr92@gmail.com>
2017-11-23 17:19:19 -0600

    test every 15 min

--- server/main.js
+++ server/main.js
@@ -8,7 +8,7 @@ Meteor.startup(() => {
     name: 'Interate every project to update google SpreadSheet',
     schedule: function(parser) {
       // 'at 5:00 pm on Mon'
-      return parser.text('every 5 mins');
+      return parser.text('every 15 mins');
     },
     job: function() {
       console.log('I am the cronjob!!')
