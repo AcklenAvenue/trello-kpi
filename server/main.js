e7156cc8bcb071943a466e5c394f94ecdec586cf
Arturo Cordova <atu.cr92@gmail.com>
2017-11-27 12:24:54 -0600

    Ping server every 15 minutes

--- server/main.js
+++ server/main.js
@@ -3,7 +3,14 @@ import '../imports/api/currentMethods';
 import '../imports/api/historyMethods';
 import '../imports/api/boardsMethods';
 
+const axios = require('axios');
+
 Meteor.startup(() => {
+  setInterval(function() {
+      console.log('ping get');
+      axios.get("https://compass-trello-kpi.herokuapp.com");
+  }, 300000); // every 5 minutes (300000)
+
   SyncedCron.add({
     name: 'Interate every project to update google SpreadSheet',
     schedule: function(parser) {
