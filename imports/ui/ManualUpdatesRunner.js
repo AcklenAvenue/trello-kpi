3298caf108ada83e2e17d8d7294283ed1e922c25
inti <inti.velasquez@acklenavenue.com>
2020-03-06 15:34:45 -0600

    Alerting user that updates are being run

--- imports/ui/ManualUpdatesRunner.js
+++ imports/ui/ManualUpdatesRunner.js
@@ -4,18 +4,21 @@ import { Meteor } from 'meteor/meteor';
 export default class ManualUpdatesRunner extends React.Component {
   
     calculateCycleTimes = () => {
+        alert('Cycle times are being calculated, it might take a couple of minutes to see the result!');
         Meteor.call('cycleTimes', (err, res) => {
             if(err) throw err;
         });
     }
 
     calculateTimeInStates = () => {
+        alert('Times in states are being estimated, it might take a couple of minutes to see the result!');
         Meteor.call('timeInStates', (err, res) => {
             if(err) throw err;
-        })
+        });
     }
 
     updateTrelloActivities = async () => {
+        alert('Webhooks are being verified, it might take a couple of minutes to see the result!');
         let boards = await new Promise((resolve, reject) => {
             Meteor.call('getAllBoards', (err, res) => {
                 if (err) reject(err);
@@ -31,11 +34,11 @@ export default class ManualUpdatesRunner extends React.Component {
     render() {
         return (
         <div className="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 text-center" id="input">
-                <button className="btn btn-primary manual-update btn-lg" onClick={this.updateTrelloActivities}>Trello Activity</button>
-                <br></br>
-                <button className="btn btn-primary manual-update btn-lg" onClick={this.calculateCycleTimes}>Calculate Cycle Times</button>
-                <br></br>
-                <button className="btn btn-primary manual-update btn-lg" onClick={this.calculateTimeInStates}>Cards Time In States</button>
+            <button className="btn btn-primary manual-update btn-lg" onClick={this.updateTrelloActivities}>Trello Activity</button>
+            <br></br>
+            <button className="btn btn-primary manual-update btn-lg" onClick={this.calculateCycleTimes}>Calculate Cycle Times</button>
+            <br></br>
+            <button className="btn btn-primary manual-update btn-lg" onClick={this.calculateTimeInStates}>Cards Time In States</button>
         </div>
         );
     };
